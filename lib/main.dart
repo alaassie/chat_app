592db6a65f3372414379ec8a0a2d2cd950b67b4b
alaassie <alaa.hm3@gmail.com>
2024-08-04 22:05:46 +0300

    firebase commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,29 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:scholar_chat/firebase_options.dart';
 import 'package:scholar_chat/pages/login_page.dart';
 import 'package:scholar_chat/pages/register_page.dart';
 
-void main() {
-  runApp(const MainApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const ChatApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class ChatApp extends StatelessWidget {
+  const ChatApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false, home: const LoginPage(), routes: {
-      RegisterPage.id: (context) => const RegisterPage(),
-      LoginPage.id: (context) => const LoginPage(),
-    });
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: const LoginPage(),
+      routes: {
+        RegisterPage.id: (context) => const RegisterPage(),
+        LoginPage.id: (context) => const LoginPage(),
+      },
+    );
   }
 }
